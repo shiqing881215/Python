5117520c9ca6318fe58568c73180df83dfd335b5
shiqing881215 <shiqing881215@gmail.com>
2015-11-02 15:06:19 -0800

    rex part2

--- advanced/rex.py
+++ advanced/rex.py
@@ -27,6 +27,7 @@ line2 = "My favorite number is 19 and 42"
 
 print re.findall("[0-9]+", line2)
 
+
 # By default, python does the greedy matching, which means get larger one
 line3 = "From: jetty's email: j@t.com"
 
@@ -34,4 +35,20 @@ line3 = "From: jetty's email: j@t.com"
 print re.findall("^F.+:", line3)  
 
 # This will print ["From:"] which is turn off default greedy setting
-print re.findall("^F.+?:", line3)  
+print re.findall("^F.+?:", line3) 
+
+# This is to extract the email address
+print re.findall("\S+@\S+", line3)
+
+
+# Sometimes, we want to match something, but don't want to extract all the matched stuff
+# So we can use (), it will only return the stuff inside of ()
+line4 = "From a@b.com saying hello"
+
+# It will match "From a@a.com", but will only return "a@a.com"
+print re.findall("^From (\S+@\S+)", line4)
+
+# Extract the domain name
+print re.findall("@(\S*)", line4)
+print re.findall("@([^ ]*)", line4)
+
